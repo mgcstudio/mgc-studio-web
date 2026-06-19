81229185cf398dfc93c25cfe364c69f74cef7bc6
MandavkarPranjal <programmer.pranjal@gmail.com>
2026-06-19 21:42:26 +0530

    add: tailscale route

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  allowedDevOrigins: ['pc.tailc1e3a1.ts.net'],
+}
 
 export default nextConfig;
